b26d722295d589b0c9ab4ad96c11f3e98bd63f29
lotero12 <lucasotero.ag@gmail.com>
2021-11-10 16:19:23 -0300

    jquery

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 class Contact {
-    constructor(name, wsp, desc) {
-      this.name = name;
-      this.wsp = wsp;
-      this.desc = desc;
-    }
+  constructor(name, wsp, desc) {
+    this.name = name;
+    this.wsp = wsp;
+    this.desc = desc;
   }
- 
+}
+
 let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 
 const getAll = () => {
-    return contacts;
-  };
+  return contacts;
+};
 const create = (newContact) => {
-    contacts.push(newContact);
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  };
-
+  contacts.push(newContact);
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+};
 
 const nameInput = $("#name-input");
 const numberWsp = $("#number-wsp");
@@ -23,21 +22,18 @@ const description = $("#description");
 const btnEnviar = $(".btn btn-primary");
 const contactList = $("contact-list");
 
-
-
 $("#enviarBtn").on("click", function () {
-    const nombre = nameInput.value;
-    const numero = numberWsp.value;
-    const descripcion = description.value;
+  const nombre = nameInput.value;
+  const numero = numberWsp.value;
+  const descripcion = description.value;
 
-    const contact = new Contact (nombre, numero, descripcion);
-    create (contact);
-    console.log (getAll()); 
-}
-)
+  const contact = new Contact(nombre, numero, descripcion);
+  create(contact);
+  console.log(getAll());
+});
 for (let contact of contacts) {
-    console.log(contact);
-    $("#contact-list").prepend(`<li>Nombre: ${contact.name} </li> <li> Tel: ${contact.wsp} </li>`);
+  console.log(contact);
+  $("#contact-list").prepend(
+    `<li>Nombre: ${contact.name} </li> <li> Tel: ${contact.wsp} </li>`
+  );
 }
-
-
